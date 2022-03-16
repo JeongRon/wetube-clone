3b5ed7ae22a3c8e1d1811992c199e8dacd71381b
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-03-16 10:43:04 +0900

    6.1
    1. Coding - watch page
    2. absoulte / relative URL
     - href="/edit"
     - href="edit"

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 // Regular Expression 활용하여 Parameters URL 만들기
 // id에는 이제 숫자만 들어갈 수 있다.
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
