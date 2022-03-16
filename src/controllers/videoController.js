4c147698b4290c95ce17275c17c425267cf490dc
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-03-16 23:33:24 +0900

    6.3
    # edit page -> change title
    - express.urlencoded()
    - videoRouter.post()
    - req.body.title / input(name="title")
    - res.redirect()

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,4 +39,11 @@ export const getEdit = (req, res) => {
   const video = videos[id - 1];
   res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  // 원래 database이면 가능하지만, 현재 가짜 database사용 중이어서 ❌
+  //video.title = title;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
