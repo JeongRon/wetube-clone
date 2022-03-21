0591d85d3a90da12587d761707511cd552d260e9
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-03-21 13:22:36 +0900

    6.8
    > check mongo installed
    And then,
    1. npm i mongoose
    2. Create / db.js file
    3. Code / db.js file
       - mongoose.connect()

---
+++ src/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+/// mongoose는 wetube라는 mongodb database를 연결해 줄 것
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+/// on은 이벤트 생길때 마다 발생 , once는 한번만 발생
+const handleOpen = () => console.log("Connected to DB ✅");
+const handleError = (error) => console.log("DB Error ❌", error);
+db.on("error", handleError);
+db.once("open", handleOpen);
