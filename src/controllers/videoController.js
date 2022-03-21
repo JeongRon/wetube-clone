812649100fc7fd2f0923071b436f00f64ed7a943
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-03-21 16:01:17 +0900

    6.11
    1. Create / init.js file
    2. Delete / fake database Code

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,50 +1,20 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home" });
 };
 export const watch = (req, res) => {
   // const id = req.params.id;
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing ${video.title}` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  // 원래 database이면 가능하지만, 현재 가짜 database사용 중이어서 ❌
-  //video.title = title;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -54,14 +24,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
