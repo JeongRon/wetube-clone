c005ebeb91936d57f8104ff1a4d75130754c3f49
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-03-23 19:29:24 +0900

    6.21
    Update / postEdit Controller
    - String.prototype.startsWith()

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -21,8 +21,19 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "❗ Video not found ❗" });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+  video.save();
   return res.redirect(`/videos/${id}`);
 };
 
@@ -36,7 +47,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
     });
     return res.redirect("/");
   } catch (error) {
