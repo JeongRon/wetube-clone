2a9d766166776b7af3ee20a2b0cf22b1879f457a
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-04-19 16:27:09 +0900

    scr 연결 에러

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -40,7 +40,7 @@ export const postEdit = async (req, res) => {
     user: { _id },
   } = req.session;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById({ _id: id });
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res
       .status(404)
